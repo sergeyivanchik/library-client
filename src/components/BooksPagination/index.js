c9a6215a5125e232787c799e1dc5167b514fe037
sergeyivanchik <ivanchik-s@mail.ru>
2020-06-05 15:27:42 +0300

    add pagination

---
+++ src/components/BooksPagination/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useDispatch, useSelector } from  'react-redux';
+
+import './index.scss';
+
+import { Pagination } from 'antd';
+
+import { setCurrentNumber } from '../../store/actions/pagination';
+
+
+const BooksPagination = () => {
+  const dispatch = useDispatch();
+  const currentNumber = useSelector(store => store.pagination.currentNumber);
+  const totalNumber = useSelector(store => store.pagination.totalNumber);
+  const books = useSelector(store => store.books.bookList);
+
+  return (
+    <div className='pagination'>
+      <Pagination
+        defaultCurrent={1}
+        current={currentNumber}
+        pageSize={totalNumber}
+        total={books && books.length}
+        onChange={pageNumber => dispatch(setCurrentNumber(pageNumber))}
+      />
+    </div>
+  );
+}
+
+export default BooksPagination;
