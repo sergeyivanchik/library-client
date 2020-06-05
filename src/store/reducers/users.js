5977fc9898499bd744a6592cf7623578a902ce76
sergeyivanchik <ivanchik-s@mail.ru>
2020-06-05 17:59:10 +0300

    add reducers for logout

--- src/store/reducers/users.js
+++ src/store/reducers/users.js
@@ -2,7 +2,9 @@ import {
   LOG_IN_SUCCESS,
   LOG_IN_FAILURE,
   CHECK_AUTHORIZATION_SUCCESS,
-  CHECK_AUTHORIZATION_FAILURE
+  CHECK_AUTHORIZATION_FAILURE,
+  LOG_OUT_SUCCESS,
+  LOG_OUT_FAILURE
 } from '../consts/users';
 
 const initialState = {
@@ -22,7 +24,13 @@ const users = (state = initialState, action) => {
         return { ...state, currentUser: action.payload };
 
     case CHECK_AUTHORIZATION_FAILURE:
-        return { ...state, error: action.payload };
+        return { ...state, error: action.payload, currentUser: null };
+
+    case LOG_OUT_SUCCESS:
+      return { ...state };
+
+    case LOG_OUT_FAILURE:
+      return { ...state, error: action.payload };
 
     default: return state;
   }
