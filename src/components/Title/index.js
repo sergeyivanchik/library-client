571b0a92a69daf7010f1d72dca296b1aa23d9652
sergeyivanchik <ivanchik-s@mail.ru>
2020-06-08 20:52:57 +0300

    create title for block

---
+++ src/components/Title/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.scss';
+
+const Title = ({ title }) => {
+  return (
+    <span className="block-title">
+      {title}
+    </span>
+  );
+}
+
+export default Title;
