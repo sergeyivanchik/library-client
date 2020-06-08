b6ebb58b2ca45100e6c857f4b74f09fc67e5c5f8
sergeyivanchik <ivanchik-s@mail.ru>
2020-06-08 21:55:42 +0300

    add reducers for signup and signup forms

--- src/store/reducers/forms.js
+++ src/store/reducers/forms.js
@@ -1,19 +1,28 @@
 import {
   SHOW_LOGIN_FORM,
-  HIDE_LOGIN_FORM
+  HIDE_LOGIN_FORM,
+  SHOW_SIGNUP_FORM,
+  HIDE_SIGNUP_FORM
 } from '../consts/forms';
 
 const initialState = {
-  showLoginForm: false
+  showLoginForm: false,
+  showSignupForm: false
 };
 
 const forms = (state = initialState, action) => {
   switch(action.type) {
     case SHOW_LOGIN_FORM:
-      return { ...state, showLoginForm: true};
+      return { ...state, showLoginForm: true, showSignupForm: false};
 
     case HIDE_LOGIN_FORM:
       return { ...state, showLoginForm: false};
+
+    case SHOW_SIGNUP_FORM:
+      return { ...state, showLoginForm: false, showSignupForm: true};
+
+    case HIDE_SIGNUP_FORM:
+      return { ...state, showSignupForm: false};
 
     default: return state;
   }
